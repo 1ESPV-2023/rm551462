333daafec41cec26971d79a9af914edbb2a5b177
daviddpacheco <rm551462@fiap.com.br>
2023-05-11 21:44:43 -0300

    calculadora

---
+++ Js/calc.js
@@ -0,0 +1,40 @@
+function insereNumeroNoVisor(botao, visor) {
+
+    const ultimoCaractere = visor.value[visor.value.length - 1];
+
+    const operadores = ['+', '-', '*', '/'];
+
+ 
+
+    if (operadores.includes(ultimoCaractere) && operadores.includes(botao.value)) {
+     visor.value = visor.value.slice(0, -1) + botao.value;
+
+    } else {
+    visor.value += botao.value;
+    }
+
+  }
+
+ 
+
+  function resultadoDasOperacoes(visor) {
+
+    visor.value = eval(visor.value);
+
+  }
+
+ 
+
+  function multiplicacao(valor, visor) {
+
+    visor.value += "*";
+
+  }
+
+ 
+
+  function divisao(valor, visor) {
+
+    visor.value += "/";
+
+  }
